ec38a80399149ad2caff500b244cf8814105e575
Foreigner444 <testingu259@gmail.com>
2025-11-06 21:00:49 +0200

    fix: lint errors

--- app/core/admin.py
+++ app/core/admin.py
@@ -10,24 +10,33 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
-    ordering = ['id']
-    list_display = ['email', 'name']
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('name',)}),
-        (
-            'Permissions',
-            {'fields': ('is_active', 'is_staff', 'is_superuser')}
-        ),
-        ('Important dates', {'fields': ('last_login',)}),
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("name",)}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
     )
-    readonly_fields = ['last_login']
+    readonly_fields = ["last_login"]
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
-    
-    
+
+
 admin.site.register(models.User, UserAdmin)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,17 +25,16 @@ class ModelTests(TestCase):
         for email, expected in sample_email:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
-            
+
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user("", "test123")
-            
+
     def test_create_superuser(self):
         """Test creating a superuser"""
         user = get_user_model().objects.create_superuser(
-            email="t0K5W@example.com",
-            password="test123"
-        ) # type: ignore
+            email="t0K5W@example.com", password="test123"
+        )  # type: ignore
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
