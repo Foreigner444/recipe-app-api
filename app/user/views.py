2ca3629ef2ae6113d1222773551c1242f3d31966
Foreigner444 <testingu259@gmail.com>
2025-11-06 23:01:49 +0200

    feat: Add User API with documentation
    - Creates a new user app with an API for creating new users.
    - Implements a CreateUserView, UserSerializer, and URL routing.
    - Adds a full test suite for the user API.
    - Integrates drf-spectacular to provide API schema and documentation.
    - Adds DRF and drf-spectacular to project dependencies.

---
+++ app/user/views.py
@@ -0,0 +1,13 @@
+"""
+Views for the User API
+"""
+
+from rest_framework import generics
+
+from user.serializer import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+
+    serializer_class = UserSerializer
