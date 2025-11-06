6ade6aeca20d028c59526750906e955992dbb4fc
Foreigner444 <testingu259@gmail.com>
2025-11-06 21:13:19 +0200

    style: Format test file to comply with flake8
    Addresses an E501 (line too long) error in
    app/core/tests/test_models.py by reformatting long lines.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,10 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful"""
         email = "t0K5W@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)  # type: ignore
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -34,7 +37,8 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser"""
         user = get_user_model().objects.create_superuser(
-            email="t0K5W@example.com", password="test123"
-        )  # type: ignore
+            email="t0K5W@example.com",
+            password="test123",
+        )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
